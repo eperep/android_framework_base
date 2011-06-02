e7b2336336990a1eb7771f3fa5442512bc713c22
Aniruddha C <aniruddhac@nvidia.com>
2011-06-02 13:05:11 +0530

    [SF Streaming] Fixing H263 playback issues
    - Changes are to fix H263 streaming playback issues.
      Corresponding GB CL is http://git-master/r/#change,31656
      Fixes Bug 807214, 806182, 831227.
    
    Reviewed-on: http://git-master/r/#change,34847
    (cherry picked from commit 0b7f618d52110f73cc73dfb74a5de389020c202c)
    
    Reviewed-on: http://git-master/r/59017
    (cherry picked from commit b29308209ae92d8289f830129a4cb10cc4d2f94d)
    Change-Id: Ib5ace8ea295572bed25253f053f1c34565973d3c
    Reviewed-on: http://git-master/r/64707
    Reviewed-by: Bharat Nihalani <bnihalani@nvidia.com>
    Tested-by: Bharat Nihalani <bnihalani@nvidia.com>

--- media/libstagefright/rtsp/AH263Assembler.cpp
+++ media/libstagefright/rtsp/AH263Assembler.cpp
@@ -100,17 +100,17 @@ ARTPAssembler::AssemblyStatus AH263Assembler::addPacket(
     }
 
     unsigned payloadHeader = U16_AT(buffer->data());
-    CHECK_EQ(payloadHeader >> 11, 0u);  // RR=0
     unsigned P = (payloadHeader >> 10) & 1;
-    CHECK_EQ((payloadHeader >> 9) & 1, 0u);  // V=0
-    CHECK_EQ((payloadHeader >> 3) & 0x3f, 0u);  // PLEN=0
-    CHECK_EQ(payloadHeader & 7, 0u);  // PEBIT=0
+    unsigned V = (payloadHeader >> 9) & 1;  // V=0
+    unsigned PLEN = (payloadHeader >> 3) & 0x3f;  // PLEN=0
+    unsigned PEBIT = (payloadHeader & 7);  // PEBIT=0
 
     if (P) {
-        buffer->data()[0] = 0x00;
-        buffer->data()[1] = 0x00;
+        buffer->data()[V + PLEN] = 0x00;
+        buffer->data()[V + PLEN + 1] = 0x00;
+        buffer->setRange(buffer->offset() + V + PLEN, buffer->size() - V - PLEN);
     } else {
-        buffer->setRange(buffer->offset() + 2, buffer->size() - 2);
+        buffer->setRange(buffer->offset() + 2 + V + PLEN, buffer->size() - 2 - V - PLEN);
     }
 
     mPackets.push_back(buffer);
