3494412bc48554576717154588562bddebb208b0
Suresh Choudhary <sureshc@nvidia.com>
2011-08-11 17:57:20 +0530

    frameworks/base: Fix crash in OMXCodec in race condition
    Fix crash when mpeg2 profile/level is unsupported due to race
    between OMXError and PortSettingsChanged event
    
    Bug 862358
    
    Reviewed-on: http://git-master/r/#change,46677
    (cherry picked from commit 18b8e6687af1afb64f2ebb652b6242354c11dbb2)
    
    Reviewed-on: http://git-master/r/59014
    (cherry picked from commit 80053140959368896a504119d86bd5cf1aa45f00)
    Change-Id: Ia17d3ea4472497a416347b2906e1be45151d09c4
    Reviewed-on: http://git-master/r/64738
    Reviewed-by: Bharat Nihalani <bnihalani@nvidia.com>
    Tested-by: Bharat Nihalani <bnihalani@nvidia.com>
    Reviewed-by: Suresh Choudhary <sureshc@nvidia.com>

--- media/libstagefright/OMXCodec.cpp
+++ media/libstagefright/OMXCodec.cpp
@@ -2898,6 +2898,10 @@ status_t OMXCodec::freeBuffer(OMX_U32 portIndex, size_t bufIndex) {
 void OMXCodec::onPortSettingsChanged(OMX_U32 portIndex) {
     CODEC_LOGV("PORT_SETTINGS_CHANGED(%ld)", portIndex);
 
+    // In case we have already got ERROR, do not proceed
+    if (mState == ERROR)
+        return;
+
     CHECK_EQ((int)mState, (int)EXECUTING);
     CHECK_EQ(portIndex, (OMX_U32)kPortIndexOutput);
     CHECK(!mOutputPortSettingsChangedPending);
